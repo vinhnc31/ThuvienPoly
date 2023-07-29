ea71277e5c0fe8595e09ee9281ec1b9209ab5de1
HOANGVANSON9999 <94769991+HOANGVANSON9999@users.noreply.github.com>
2023-07-29 15:20:56 +0700

    themRadioButton

--- components/phieu_muon/them_phieu_muon.js
+++ components/phieu_muon/them_phieu_muon.js
@@ -1,172 +1,254 @@
 import React, { useState } from "react";
 import {
-    View,
-    Text,
-    StyleSheet,
-    StatusBar,
-    Dimensions,
-    SafeAreaView,
-    Image,
-    TouchableOpacity,
+  View,
+  Text,
+  StyleSheet,
+  StatusBar,
+  Dimensions,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
 } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
-import { TextInput } from "react-native-paper";
+import { TextInput,RadioButton } from "react-native-paper";
 
 const { widthSrc, heightSrc } = Dimensions.get("window");
-
+const handleRadioPress = () => {};
 const them_phieu_muon = ({ navigation }) => {
-
-    return (
-        <View>
-            <StatusBar barStyle="light-content" />
-            <SafeAreaView style={{ backgroundColor: '#FC6011' }}>
-                <View style={styles.container}>
-                    {/* phần header */}
-                    <View style={styles.header}>
-                        <TouchableOpacity onPress={() => navigation.goBack()}>
-                            <Image source={require("../../img/Icon/go_back.png")} />
-                        </TouchableOpacity>
-                        <Text style={{ fontSize: 18, fontWeight: "bold", color: "white" }}>Thêm Phiếu mượn</Text>
-                        <View></View>
-                    </View>
-                    {/* phần body */}
-                    <ScrollView>
-                        {/* PHần thông tin user */}
-                        <View style={styles.infor_user}>
-                            <View style={styles.header_infor_user}>
-                                <Text style={{ fontSize: 16, fontWeight: "bold", }}>Thông tin người mượn</Text>
-                                <Image source={require('../../img/Icon/search.png')} />
-                            </View>
-                            <View >
-                                <View style={{ width: '100%', marginBottom: 16 }}>
-                                    <Text>Họ và Tên</Text>
-                                    <TextInput placeholder="Nhập họ và tên" style={styles.textInput}></TextInput>
-                                </View>
-
-                                <View style={{ width: '100%', marginBottom: 16 }}>
-                                    <Text>Ngày sinh</Text>
-                                    <TextInput placeholder="Chọn ngày sinh" style={styles.textInput}></TextInput>
-                                </View>
-
-                                <View style={{ width: '100%', marginBottom: 16 }}>
-                                    <Text>Địa chỉ</Text>
-                                    <TextInput placeholder="Nhập địa chỉ" style={styles.textInput}></TextInput>
-                                </View>
-
-                                <View style={{ width: '100%', marginBottom: 16 }}>
-                                    <Text>Số điện thoại</Text>
-                                    <TextInput placeholder="Nhập số điện thoại" style={styles.textInput} keyboardType="phone-pad" maxLength={11}></TextInput>
-                                </View>
-                                <View style={{ width: '100%', marginBottom: 16 }}>
-                                    <Text>Email</Text>
-                                    <TextInput placeholder="Nhập Email" style={styles.textInput} keyboardType="email-address"></TextInput>
-                                </View>
-                            </View>
-                        </View>
-
-                        {/* phần thông tin sách */}
-                        <View style={styles.infor_user}>
-                            <View style={styles.header_infor_user}>
-                                <Text style={{ fontSize: 16, fontWeight: "bold", }}>Thông tin sách mượn</Text>
-                                <Image source={require('../../img/Icon/search.png')} />
-                            </View>
-                            <View >
-                                <View style={{ width: '100%', marginBottom: 16, }}>
-                                    <Text>Tên sách</Text>
-                                    <TextInput placeholder="Nhập họ và tên" style={styles.textInput}></TextInput>
-                                </View>
-
-                                <View style={{ flexDirection: "row", marginBottom: 16, maxWidth: '100%', justifyContent: "space-between" }}>
-                                    <View style={{ width: '49%' }}>
-                                        <Text >Tác giả</Text>
-                                        <TextInput placeholder="Chọn ngày sinh" style={styles.textInput}></TextInput>
-                                    </View>
-
-                                    <View style={{ width: '49%' }}>
-                                        <Text>Nhà xuất bản</Text>
-                                        <TextInput placeholder="Nhập địa chỉ" style={styles.textInput}></TextInput>
-                                    </View>
-                                </View>
-
-                                <View style={{ flexDirection: "row", marginBottom: 16, maxWidth: '100%', justifyContent: "space-between" }}>
-                                    <View style={{ width: '49%' }}>
-                                        <Text >Ngày mượn</Text>
-                                        <TextInput placeholder="Chọn ngày sinh" style={styles.textInput}></TextInput>
-                                    </View>
-
-                                    <View style={{ width: '49%' }}>
-                                        <Text>Ngày trả</Text>
-                                        <TextInput placeholder="Nhập địa chỉ" style={styles.textInput}></TextInput>
-                                    </View>
-                                </View>
-
-                            </View>
-                        </View>
-
-                        <TouchableOpacity style={{ justifyContent: "center", alignItems: "center", backgroundColor: '#FC6011', paddingTop: 16, paddingBottom: 16, marginStart: 32, marginEnd: 32, borderRadius: 50, marginBottom: 16, marginTop: -8 }}>
-                            <Text style={{ color: 'white', fontSize: 16, fontWeight: "500", }}>Hoàn thành</Text>
-                        </TouchableOpacity>
-
-                    </ScrollView>
+  return (
+    <View>
+      <StatusBar barStyle="light-content" />
+      <SafeAreaView style={{ backgroundColor: "#FC6011" }}>
+        <View style={styles.container}>
+          {/* phần header */}
+          <View style={styles.header}>
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+              <Image source={require("../../img/Icon/go_back.png")} />
+            </TouchableOpacity>
+            <Text style={{ fontSize: 18, fontWeight: "bold", color: "white" }}>
+              Thêm Phiếu mượn
+            </Text>
+            <View></View>
+          </View>
+          {/* phần body */}
+          <ScrollView>
+            {/* PHần thông tin user */}
+            <View style={styles.infor_user}>
+              <View style={styles.header_infor_user}>
+                <Text style={{ fontSize: 16, fontWeight: "bold" }}>
+                  Thông tin người mượn
+                </Text>
+                <Image source={require("../../img/Icon/search.png")} />
+              </View>
+              <View>
+                <View style={{ width: "100%", marginBottom: 16 }}>
+                  <Text>Họ và Tên</Text>
+                  <TextInput
+                    placeholder="Nhập họ và tên"
+                    style={styles.textInput}
+                  ></TextInput>
                 </View>
-            </SafeAreaView>
+
+                <View style={{ width: "100%", marginBottom: 16 }}>
+                  <Text>Ngày sinh</Text>
+                  <TextInput
+                    placeholder="Chọn ngày sinh"
+                    style={styles.textInput}
+                  ></TextInput>
+                </View>
+
+                <View style={{ width: "100%", marginBottom: 16 }}>
+                  <Text>Địa chỉ</Text>
+                  <TextInput
+                    placeholder="Nhập địa chỉ"
+                    style={styles.textInput}
+                  ></TextInput>
+                </View>
+
+                <View style={{ width: "100%", marginBottom: 16 }}>
+                  <Text>Số điện thoại</Text>
+                  <TextInput
+                    placeholder="Nhập số điện thoại"
+                    style={styles.textInput}
+                    keyboardType="phone-pad"
+                    maxLength={11}
+                  ></TextInput>
+                </View>
+                <View style={{ width: "100%", marginBottom: 16 }}>
+                  <Text>Email</Text>
+                  <TextInput
+                    placeholder="Nhập Email"
+                    style={styles.textInput}
+                    keyboardType="email-address"
+                  ></TextInput>
+                </View>
+
+                <View style={{ width: "100%", marginBottom: 16 }}>
+                  <Text>Giới tính</Text>
+                  <View>
+                    <RadioButton.Group onValueChange={handleRadioPress}>
+                      <View>
+                        <View
+                          style={{ flexDirection: "row", alignItems: "center" }}
+                        >
+                          <RadioButton value="nam" />
+                          <Text>Nam</Text>
+
+                          <RadioButton value="nu" />
+                          <Text>Nữ</Text>
+                        </View>
+                      </View>
+                    </RadioButton.Group>
+                  </View>
+                </View>
+              </View>
+            </View>
+
+            {/* phần thông tin sách */}
+            <View style={styles.infor_user}>
+              <View style={styles.header_infor_user}>
+                <Text style={{ fontSize: 16, fontWeight: "bold" }}>
+                  Thông tin sách mượn
+                </Text>
+                
+              </View>
+              <View>
+                <View style={{ width: "100%", marginBottom: 16 }}>
+                  <Text>Tên sách</Text>
+                  <TextInput
+                    placeholder="Nhập họ và tên"
+                    style={styles.textInput}
+                  ></TextInput>
+                </View>
+
+                <View
+                  style={{
+                    flexDirection: "row",
+                    marginBottom: 16,
+                    maxWidth: "100%",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <View style={{ width: "49%" }}>
+                    <Text>Tác giả</Text>
+                    <TextInput
+                      placeholder="Chọn ngày sinh"
+                      style={styles.textInput}
+                    ></TextInput>
+                  </View>
+
+                  <View style={{ width: "49%" }}>
+                    <Text>Nhà xuất bản</Text>
+                    <TextInput
+                      placeholder="Nhập địa chỉ"
+                      style={styles.textInput}
+                    ></TextInput>
+                  </View>
+                </View>
+
+                <View
+                  style={{
+                    flexDirection: "row",
+                    marginBottom: 16,
+                    maxWidth: "100%",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <View style={{ width: "49%" }}>
+                    <Text>Ngày mượn</Text>
+                    <TextInput
+                      placeholder="Chọn ngày sinh"
+                      style={styles.textInput}
+                    ></TextInput>
+                  </View>
+
+                  <View style={{ width: "49%" }}>
+                    <Text>Ngày trả</Text>
+                    <TextInput
+                      placeholder="Nhập địa chỉ"
+                      style={styles.textInput}
+                    ></TextInput>
+                  </View>
+                </View>
+              </View>
+            </View>
+
+            <TouchableOpacity
+              style={{
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "#FC6011",
+                paddingTop: 16,
+                paddingBottom: 16,
+                marginStart: 32,
+                marginEnd: 32,
+                borderRadius: 50,
+                marginBottom: 16,
+                marginTop: -8,
+              }}
+            >
+              <Text style={{ color: "white", fontSize: 16, fontWeight: "500" }}>
+                Hoàn thành
+              </Text>
+            </TouchableOpacity>
+          </ScrollView>
         </View>
-    );
+      </SafeAreaView>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        maxHeight: '100%',
-        backgroundColor: "#F0F5F9",
-    },
+  container: {
+    maxHeight: "100%",
+    backgroundColor: "#F0F5F9",
+  },
 
-    header: {
-        backgroundColor: '#FC6011',
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: 'space-between',
-        paddingTop: 32,
-        paddingBottom: 16,
-        marginBottom: 16,
-        paddingStart: 8,
-        paddingEnd: 16,
-    },
+  header: {
+    backgroundColor: "#FC6011",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingTop: 32,
+    paddingBottom: 16,
+    marginBottom: 16,
+    paddingStart: 8,
+    paddingEnd: 16,
+  },
 
-    body: {
-        backgroundColor: '#fff',
-        flexDirection: "column",
-    },
+  body: {
+    backgroundColor: "#fff",
+    flexDirection: "column",
+  },
 
-    header_infor_user: {
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-        paddingStart: 18,
-        paddingEnd: 18,
-        marginTop: 16,
-        marginBottom: 16,
-    },
+  header_infor_user: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingStart: 18,
+    paddingEnd: 18,
+    marginTop: 16,
+    marginBottom: 16,
+  },
 
-    infor_user: {
-        paddingStart: 18,
-        paddingEnd: 18,
-        marginBottom: 32,
-        backgroundColor: '#fff'
-    },
+  infor_user: {
+    paddingStart: 18,
+    paddingEnd: 18,
+    marginBottom: 32,
+    backgroundColor: "#fff",
+  },
 
-    textInput: {
-        fontSize: 13,
-        color: "#ffffff",
-        borderRadius: 6,
-        width: widthSrc,
-        height: 48,
-        backgroundColor: "none",
-        marginTop: 10,
-        borderColor: '#FC6011',
-        borderWidth: 1,
-        cursorColor: '#FC6011',
-    },
-
-
+  textInput: {
+    fontSize: 13,
+    color: "#ffffff",
+    borderRadius: 6,
+    width: widthSrc,
+    height: 48,
+    backgroundColor: "none",
+    marginTop: 10,
+    borderColor: "#FC6011",
+    borderWidth: 1,
+    cursorColor: "#FC6011",
+  },
 });
 export default them_phieu_muon;
